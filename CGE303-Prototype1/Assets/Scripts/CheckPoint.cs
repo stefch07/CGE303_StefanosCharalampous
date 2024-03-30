6d03063958a7d5ebb04486d98e644dcc04acc797
stefch07 <schar9360@365.elmhurst.edu>
2024-03-30 15:28:13 -0500

    .

---
+++ CGE303-Prototype1/Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    private GameMaster gm;
+
+    void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            gm.lastCheckPointPos = transform.position;
+        }
+    }
+}
